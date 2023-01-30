f390d2a63764d355ebbfb80934d2b58531b55405
FrankQQQQ <18518553618@163.com>
2023-01-29 20:45:57 -0500

    uodate type from int to string. Update get balance factor

--- AVL_Tree_init.cpp
+++ AVL_Tree_init.cpp
@@ -1,16 +1,18 @@
 #include<iostream>
 using namespace std;
+#include <vector>
+#include <string>
 
 
 class Node{
     // Node Class
     public:
-    int key;
-    int val;
+    string key;
+    string val;
     int factor;
     Node * left;
     Node * right;
-    Node(int key, int value){
+    Node(string key, string value){
         this->key = key;
         this->val = value;
         this->left = nullptr;
@@ -43,7 +45,7 @@ class Tree{
         return (1 + getNodeNum(root->left) + getNodeNum(root->right));
     }
 
-    Node* insert(Node *root, int key, int value){
+    Node* insert(Node *root, string key, string value){
         Node * node = new Node(key, value);
         if (root == NULL){
             return node;
@@ -90,20 +92,52 @@ void printTree(Node* root){
             return;
         }
         printTree(root->left);
-        cout << root->val;
+        cout << ("[" + root->key +": "+ root->val + "]");
         printTree(root->right);
     }
 
+
+vector<vector<Node*>> levelOrder(Node * root){
+    // cout << "start";
+    vector<Node*> q;
+    q.push_back(root);
+    vector<vector<Node*>> result;
+    while (! q.empty()){
+        vector<Node*> k;
+        result.push_back(k);
+        for (int i = 0; i < q.size(); i += 1){
+            Node * cur = q[0];
+            q.erase(q.begin());
+            result[result.size() - 1].push_back(cur);
+            if (cur->left != NULL){
+                q.push_back(cur->left);
+            }
+            if (cur->right != NULL){
+                q.push_back(cur->right);
+            }
+            
+        }
+    }
+    for (int i = 0; i < result.size(); i += 1){
+        
+        for (int j = 0; j < result[i].size(); j += 1){
+            cout << result[i][j]->key;
+        }
+        cout << "\n";
+
+    }
+    return result;
+}
+
 int main(){
-    Node* root = new Node(1,4);
-    Node* a = new Node(2,6);
-    Node* b = new Node(3,8);
+    Node* root = new Node("1","4");
+    Node* a = new Node("2","6");
+    Node* b = new Node("3","8");
     root->left = a;
     root->right = b;
-    // printTree(root);
+    printTree(root);
 
     Tree* my_tree = new Tree(root, 100);
-    cout << my_tree->getNodeNum(root);
-    cout << "\n";
-    cout << my_tree->getHeight(root->left);
+    // cout << my_tree->getNodeNum(root);
+    
 }
